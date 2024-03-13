e52f9fe256b080c21f59428ad6f29c7a76fa09cf
Gabriel Albuquerque <gabrieldafortaleza0100@gmail.com>
2024-03-13 11:24:16 -0300

    Alteração no atributo type dos inputs, responsividade implementada e melhoria do código javascript

--- src/scripts/form.js
+++ src/scripts/form.js
@@ -1,22 +1,18 @@
 const itensFormulario = document.querySelectorAll('.items-form')
 const botaoEnviar = document.getElementById('botao-enviar')
 const alertaCampoNaoPreenchido = document.querySelectorAll('.campo-obrigatorio')
-itensFormulario.forEach(function(item,i) {
-    item.addEventListener('change',() => {
-        if(item.value !== ""){
-        item.classList.add("borda-campo-preenchido")
-        alertaCampoNaoPreenchido[i].classList.remove("alerta-campo-nao-preenchido")
-        }
-})
-})
-botaoEnviar.addEventListener('click',() => {    
-    itensFormulario.forEach(function(item,i){
-    if(item.value === ""){
-        if(item.classList.contains("borda-campo-preenchido")){
-            item.classList.remove("borda-campo-preenchido")
-        }
-        item.classList.add("borda-campo-nao-preenchido")
-        alertaCampoNaoPreenchido[i].classList.add("alerta-campo-nao-preenchido")     
+
+botaoEnviar.addEventListener('click',(e) => {
+    e.preventDefault()
+    itensFormulario.forEach(function(input,i){
+    if(input.value){
+        input.classList.add('valido')
+        alertaCampoNaoPreenchido[i].classList.remove('mostrar')     
+    }else{
+        input.classList.remove('valido')
+        input.classList.add('erro')
+        alertaCampoNaoPreenchido[i].classList.add('mostrar')
+        
     }     
     })
 })
